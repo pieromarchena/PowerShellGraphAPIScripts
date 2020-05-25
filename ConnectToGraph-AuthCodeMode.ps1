5ac7d55bdc1cb23dc4f073526ab33acf9a1db848
Piero Marchena <pmarchena88@hotmail.com>
2020-05-24 20:38:56 -0500

    Add files via upload

---
+++ ConnectToGraph-AuthCodeMode.ps1
@@ -0,0 +1,68 @@
+﻿# The resource URI
+$resource = "https://graph.microsoft.com"
+# Your Client ID and Client Secret obainted when registering your WebApp
+$clientid = "13da4e98-1f6f-4e01-b6ae-68b79d477669"
+$clientSecret = "pq2BH20~c12H~SIbRX9I5~_5Ai_1R~Cn-d"
+$redirectUri = "https://www.pieromarchena.com"
+
+# UrlEncode the ClientID and ClientSecret and URL's for special characters 
+Add-Type -AssemblyName System.Web
+$clientIDEncoded = [System.Web.HttpUtility]::UrlEncode($clientid)
+$clientSecretEncoded = [System.Web.HttpUtility]::UrlEncode($clientSecret)
+$redirectUriEncoded =  [System.Web.HttpUtility]::UrlEncode($redirectUri)
+$resourceEncoded = [System.Web.HttpUtility]::UrlEncode($resource)
+$scopeEncoded = [System.Web.HttpUtility]::UrlEncode("https://outlook.office.com/user.readwrite.all")
+
+# Function to popup Auth Dialog Windows Form
+Function Get-AuthCode {
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $form = New-Object -TypeName System.Windows.Forms.Form -Property @{Width=440;Height=640}
+    $web  = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{Width=420;Height=600;Url=($url -f ($Scope -join "%20")) }
+
+    $DocComp  = {
+        $Global:uri = $web.Url.AbsoluteUri        
+        if ($Global:uri -match "error=[^&]*|code=[^&]*") {$form.Close() }
+    }
+    $web.ScriptErrorsSuppressed = $true
+    $web.Add_DocumentCompleted($DocComp)
+    $form.Controls.Add($web)
+    $form.Add_Shown({$form.Activate()})
+    $form.ShowDialog() | Out-Null
+
+    $queryOutput = [System.Web.HttpUtility]::ParseQueryString($web.Url.Query)
+    $output = @{}
+    foreach($key in $queryOutput.Keys){
+        $output["$key"] = $queryOutput[$key]
+    }
+
+    $output
+}
+
+# Get AuthCode
+$url = "https://login.microsoftonline.com/common/oauth2/authorize?response_type=code&redirect_uri=$redirectUriEncoded&client_id=$clientID&resource=$resourceEncoded&prompt=admin_consent&scope=$scopeEncoded"
+Get-AuthCode
+# Extract Access token from the returned URI
+$regex = '(?<=code=)(.*)(?=&)'
+$authCode  = ($uri | Select-string -pattern $regex).Matches[0].Value
+
+Write-output "Received an authCode, $authCode"
+
+
+#get Access Token
+$body = "grant_type=authorization_code&redirect_uri=$redirectUri&client_id=$clientId&client_secret=$clientSecretEncoded&code=$authCode&resource=$resource"
+$tokenResponse = Invoke-RestMethod https://login.microsoftonline.com/common/oauth2/token `
+    -Method Post -ContentType "application/x-www-form-urlencoded" `
+    -Body $body `
+    -ErrorAction STOP
+
+#6) List the published apps from the Microsoft Teams app Catalog
+
+#$apiUrl = "https://graph.microsoft.com/v1.0/appCatalogs/teamsApps?$filter=distributionMethod eq 'organization'"
+$apiUrl = "https://graph.microsoft.com/v1.0/appCatalogs/teamsApps"
+
+$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method Get
+
+$ListOfApps = ($Data | select-object Value).Value | Select-Object displayName, id, externalId, distributionMethod
+
+$ListOfApps | export-csv -notypeinformation -encoding 'utf8' -path ('.\TeamsApps.csv')
---
+++ ConnectToGraph-ClientMethod.ps1
@@ -0,0 +1,98 @@
+﻿#Documentation:
+# https://docs.microsoft.com/en-us/graph/api/group-post-groups?view=graph-rest-1.0&tabs=http
+
+# Application (client) ID, tenant Name and secret
+$clientId = "13da4e98-1f6f-4e01-b6ae-68b79d477669"
+$tenantName = "M365x298804.onmicrosoft.com"
+$clientSecret = "pq2BH20~c12H~SIbRX9I5~_5Ai_1R~Cn-d"
+$resource = "https://graph.microsoft.com/"
+
+
+$ReqTokenBody = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    client_Id     = $clientID
+    Client_Secret = $clientSecret
+} 
+
+$TokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantName/oauth2/v2.0/token" -Method POST -Body $ReqTokenBody
+
+#SAMPLES:
+
+#1) Get All Groups:
+
+#$apiUrl = 'https://graph.microsoft.com/v1.0/Groups/'
+#$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method Get
+#$Groups = ($Data | select-object Value).Value
+#$Groups | Format-Table DisplayName, Description -AutoSize
+
+#2) List All Teams:
+
+#$apiUrl = "https://graph.microsoft.com/beta/groups?`$filter=resourceProvisioningOptions/Any(x:x eq 'Team')"
+#$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method Get
+#$Teams = ($Data | select-object Value).Value | Select-Object displayName, id, description
+#$Teams
+
+#3) List All Team Channels:
+
+#$apiUrl = "https://graph.microsoft.com/beta/teams/d386ef45-b535-4a60-a683-4fd9a55ca4d8/channels"
+#$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method Get
+#$TeamChannels = ($Data | select-object Value).Value
+#$TeamChannels
+
+#4) Post to Channel:
+#$apiUrl = "https://graph.microsoft.com/beta/teams/d386ef45-b535-4a60-a683-4fd9a55ca4d8/channels/19:51454a08de90466db714a0d3e8efeb90@thread.tacv2/messages"
+#$body = @"
+#{ 
+#"body": { 
+#    "content": "Hello Management team! This is being sent to you from PowerShell!" 
+#     } 
+# }
+#"@
+
+#Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Body $body -Method Post -ContentType 'application/json'
+
+#5) Create a Group:
+#$apiUrl = "https://graph.microsoft.com/v1.0/groups"
+
+#$body = @"
+#{
+#  "description": "Self help community for library",
+#  "displayName": "Library Assist",
+#  "groupTypes": [
+#    "Unified"
+#  ],
+#  "mailEnabled": true,
+#  "mailNickname": "library",
+#  "securityEnabled": false
+#}
+#"@
+#Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Body $body -Method Post -ContentType 'application/json'
+
+#6) List channel messages
+
+#$teamsid= "d386ef45-b535-4a60-a683-4fd9a55ca4d8";
+#$channelid= "19:51454a08de90466db714a0d3e8efeb90@thread.tacv2";
+
+#$apiUrl = "https://graph.microsoft.com/beta/teams/" + $teamsid + "/channels/" + $channelid + "/messages"
+
+#$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method Get
+
+#$Messages = ($Data | select-object Value).Value | Select-Object id, messageType, createdDateTime, importance, mentions
+
+#$Messages
+
+#$ListOfApps | export-csv -notypeinformation -encoding 'utf8' -path ('.\TeamsApps.csv')
+
+
+#7) Get Members of a Team
+
+$teamsid = "d386ef45-b535-4a60-a683-4fd9a55ca4d8";
+
+$apiUrl = "https://graph.microsoft.com/v1.0/groups/" + $teamsid  + "/members";
+
+$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method Get
+
+$members = ($Data | select-object Value).Value | Select-Object id,displayName,jobTitle,mail,mobilePhone,officeLocation,userPrincipalName,businessPhones
+
+$members | export-csv -notypeinformation -encoding 'utf8' -path ('.\TeamsMembers.csv')
